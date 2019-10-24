910b2eabfac95c781e1f457c36ab046bac065236
shintabella <shintabella12smktrisakti@gmail.com>
2019-10-24 14:45:04 +0700

    servicebella

---
+++ app/src/main/java/com/yogiapp/service/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.yogiapp.service
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.media.MediaPlayer
+import android.net.Uri
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_facebook.setOnClickListener({
+            val i = Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://www.facebook.com"))
+            startActivity(i)
+        })
+        btn_instagram.setOnClickListener({
+            val i = Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://www.Instagram.com"))
+            startActivity(i)
+        })
+        var MediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.lily)
+        btn_play.setOnClickListener {
+            MediaPlayer?.start()
+        }
+        btn_pause.setOnClickListener {
+            MediaPlayer?.pause()
+        }
+        btn_stop.setOnClickListener {
+            MediaPlayer?.pause()
+            MediaPlayer?.seekTo(0)
+        }
+    }
+}
